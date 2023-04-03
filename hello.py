5b92a8b9e26edd0adaf662f804a39a6cd3550ef9
Mainul I. Sunvi <mainulsunvi@gmail.com>
2023-04-03 19:12:54 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ def add(a, b):
     return a + b
 
 print("The addition of Two Number is: ")
-print(add(50, 40))
+print(add(50, 50))
 
 print("Hello World")
 
-print("Jenkins is best")
+print("Jenkins is best !!")
